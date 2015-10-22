852b2f109573cfae57fa112f0ecb0f1d528cbf6d
Sean Thompson <sean.thompson@rootmetrics.com>
2015-10-22 09:32:32 -0700

    Fixing enter and exit on bar charts

--- src/js/rm-bar-chart.js
+++ src/js/rm-bar-chart.js
@@ -18,6 +18,8 @@ var rmBarChart = function() {
 		cheight,
 		y,
 		yAxis,
+		x,
+		xAxis,
 		animationComplete = false;
 
 	
@@ -51,37 +53,136 @@ var rmBarChart = function() {
 		svg.call(chart.animate);
 	}
 	
-	chart.animate = function(svg) {		
+	chart.animate = function(svg) {	
+		barWidth = cwidth / dataset.length;
+	  	recWidth = barWidth / 2;
+			
 		var barContent = svg.select('.bar-content');							
-		var bars = barContent.selectAll('.carrier-bar');	
-		var scores = barContent.selectAll('.scoreText');
-		var ranks = barContent.selectAll('.rankText');
-		var rankCircles = barContent.selectAll('.rankCircle');
-		var errorBottom = barContent.selectAll('.error-bar-bottom');
-		var errorTop = barContent.selectAll('.error-bar-top');
-		var errorBar = barContent.selectAll('.error-bar');
-		var carrierText = barContent.selectAll('.carrierText');
-
+		var bars = barContent.selectAll('.carrier-bar').data(dataset);	
+		var scores = barContent.selectAll('.scoreText').data(dataset);
+		var ranks = barContent.selectAll('.rankText').data(dataset);
+		var rankCircles = barContent.selectAll('.rankCircle').data(dataset);
+		var errorBar = barContent.selectAll('.error-bar').data(dataset);
+		var carrierText = barContent.selectAll('.carrierText').data(dataset);
+		var xLines = xAxis.selectAll(".xaxis").data(dataset);		
 		var upperMax = d3.extent(dataset, function(d) { return d.upper; })[1];
 
 		y.domain([min, upperMax]);
 		svg.select(".y.axis").transition().duration(500).call(yAxis);
 		
-		bars.data(dataset)
+		svg.select('.x.axis')
+			.attr('transform', 'translate(' + [barWidth, 0] + ')');
+		
+		xLines.exit().remove();
+		
+		xLines.enter()
+			.append("line")
+			.attr("class", "xaxis")
+			.attr("transform", function(d,i) {
+				return "translate(" + ((i * barWidth) ) + ", 0)"
+			})
+			.attr("x1", 0)					
+			.attr("x2", 0)
+			.attr("y1", 0)	
+			.attr("y2", cheight);	
+		
+		xLines.transition()
+			.duration(500)
+			.attr("class", "xaxis")
+			.attr("transform", function(d,i) {
+				return "translate(" + ((i * barWidth) ) + ", 0)"
+			})
+			.attr("x1", 0)					
+			.attr("x2", 0)
+			.attr("y1", 0)	
+			.attr("y2", cheight);					
+	
+		bars.exit().remove();					
+								
+		bars.enter()
+			.append('rect')
 			.attr("class", function(d) { return d.name })
 			.classed('carrier-bar', true)
-			.transition()
+			.attr("y", cheight)
+			.attr("transform", function(d, i) { return "translate(" + ((i * barWidth) + (barWidth/2) - recWidth/2) + ",0)"; })
+			.attr("height", 0);
+			
+		bars.transition()	
 			.duration(500)
 			.delay(function(d, i) { return 100 * i })
 			.ease('exp-out')
-			.attr("height", function(d) { return cheight - y(d.score); })			
-			.attr("y", function(d) { return y(d.score); })		
-		
-		carrierText.data(dataset)
-			.text(function(d){return d.name;});
+			.attr("transform", function(d, i) { return "translate(" + ((i * barWidth) + (barWidth/2) - recWidth/2) + ",0)"; })			
+			.attr("height", function(d) { return cheight - y(d.score); })	
+			.attr("width", recWidth)		
+			.attr("y", function(d) { return y(d.score); });	
 			
-		scores.data(dataset)
+		
+		if(errorBars) { 
+			errorBar.exit().remove();
+							
+			errorBar.enter()
+					.append("rect")
+					.attr("class", function(d) { return d.name })
+					.classed("error-bar secondary", true)
+					.attr("width", 2)
+					.attr("transform", function(d, i) { 
+						return "translate(" + ((i * barWidth) + (barWidth/2)) + "," + cheight + ")"; })
+					.attr("height", 0);
+					
+			errorBar.transition()
+					.duration(500)
+					.delay(function(d, i) { return 100 * i})
+					.ease('exp-out')
+					.attr("transform", function(d, i) {
+						var upperScale = y(d.upper);
+						return "translate(" + ((i * barWidth) + (barWidth/2)) + "," + upperScale + ")"; 
+					})
+					.attr("height", function(d) {
+						var upperScale = y(d.upper);
+						var lowerScale = y(d.lower);
+						return  (lowerScale - upperScale);   
+					});		
+		};	
+		
+		carrierText.exit().remove();								
+		
+		carrierText.enter()
+	            .append("text")
+	            .style("font-size", "12px")
+	            .attr("class", "carrierText")
+	            .style("fill", "#555555")
+	            .attr("y", cheight + 20)
+	            .style("text-anchor", "middle")
+	            .attr("transform", function(d, i) { 
+					return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; 
+	            })
+	            .text(function(d){return d.name;});	
+						
+		carrierText
 			.transition()
+			.duration(500)
+			.delay(function(d, i) { return 100 * i })
+			.text(function(d){return d.name;})
+			.attr("y", cheight + 20)
+            .style("text-anchor", "middle")
+			.attr("transform", function(d, i) { 
+				return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; 
+            });							
+		
+		scores.exit().remove();  
+			
+		scores.enter()
+            .append("text")
+            .attr("class", "scoreText")
+            .attr("text-anchor", "middle")
+            .style("fill", "#555")
+            .style("font-size", "12px")
+            .attr("transform", function(d, i) { 
+		        return "translate(" + ((i * barWidth) + (barWidth/2)) + "," +  cheight + ")"; 
+             })   
+            .text(function(d) { return d.score });	
+			
+		scores.transition()
 			.duration(500)
 			.delay(function(d, i) { return 100 * i})
 			.ease('exp-out')
@@ -93,47 +194,35 @@ var rmBarChart = function() {
             	} 
 			})             
             .text(function(d) { return d.score });
-            
-        errorBar.data(dataset)
-        	.attr("class", function(d) { return d.name })
-			.classed("error-bar secondary", true)
-			.transition()
-			.duration(500)
-			.delay(function(d, i) { return 100 * i})
-			.ease('exp-out')
-			.attr("transform", function(d, i) {
-				var upperScale = y(d.upper);
-				return "translate(" + ((i * barWidth) + (barWidth/2)) + "," + upperScale + ")"; 
-			})
-			.attr("height", function(d) {
-				var upperScale = y(d.upper);
-				var lowerScale = y(d.lower);
-				return  (lowerScale - upperScale);   
-			})
-            
-        errorBottom.data(dataset)
-			.transition()
-			.duration(500)
-			.delay(function(d, i) { return 100 * i})
-			.ease('exp-out')
-			.attr("transform", function(d, i) {
-				var lowerScale = y(d.lower);
-				return "translate(" + ((barWidth*i) + (barWidth/2) - (recWidth/4)) + "," + lowerScale + ")"; 
-			});					
-		
-		errorTop.data(dataset)
-			.transition()
-			.duration(500)
-			.delay(function(d, i) { return 100 * i})
-			.ease('exp-out')
-			.attr("transform", function(d, i) {
-				var upperScale = y(d.upper);
-				return "translate(" + ((barWidth*i) + (barWidth/2) - (recWidth/4)) + "," + upperScale + ")";
-			});		
+           
 			
 		if(ranks) {
-	        rankCircles.data(dataset)
+			
+			rankCircles.exit().remove();
+	       
+			rankCircles.enter()
+                .append("circle")
                 .attr('class', "rankCircle")
+                .attr("fill", function(d) {
+					if(d.rank == 1) {
+						return "#555555";
+					} else {
+						return "transparent";
+					}
+				})
+				.attr("stroke", function(d) {
+					if(d.rank == 1) {
+						return "#555555";
+					} else {
+						return "#999999";
+					}
+				})
+                .attr("stroke-width", "1px")
+                .attr("cy", cheight + 40)
+                .attr("r", 10)
+                .attr("transform", function(d, i) { return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; }) 	
+ 	       
+	        rankCircles.attr('class', "rankCircle")
                 .style("fill", function(d) {
 					if(d.rank == 1) {
 						return "#555555";
@@ -147,11 +236,48 @@ var rmBarChart = function() {
 					} else {
 						return "#999999";
 					}
-				});
-	        
-	    	ranks.data(dataset)           
-	             .text(function(d) { return d.rank });
-        }   	
+				})
+				.transition()
+				.duration(500)
+				.attr("transform", function(d, i) { return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; }) 
+			
+			
+			ranks.exit().remove();
+	       
+			ranks.enter()
+	            .append("text")
+	            .style("font-size", "12px")
+	            .attr("class", "rankText")
+	            .style("fill", function(d) {
+					if(d.rank == 1) {
+						return "#FFFFFF";
+					} else {
+						return "#555555";
+					}
+				})
+	            .attr("y", cheight + 44)
+	            .style("text-anchor", "middle")
+	            .attr("transform", function(d, i) { 
+					return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; 
+	            })
+	            .text(function(d, i){ return d.rank; });
+			
+	        ranks.transition()
+				 .duration(500)
+	             .attr("transform", function(d, i) { 
+					return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; 
+	             })
+	             .style("fill", function(d) {
+					if(d.rank == 1) {
+						return "#FFFFFF";
+					} else {
+						return "#555555";
+					}
+				})
+				.text(function(d) { return d.rank })       	             	       	       		        	        	        	        	    		             
+	               
+        } 
+ 	
 			             
 	};	
 	
@@ -183,147 +309,12 @@ var rmBarChart = function() {
 			g.select(".y.axis").call(yAxis);
 			
 		
-		var xAxis = g.append("g")
+		xAxis = g.append("g")
 		    .attr("class", "x axis")
-		    .attr('transform', 'translate(' + [barWidth, 0] + ')');
-		
-		xAxis.selectAll(".xaxis")
-			.data(dataset)
-			.enter()
-			.append("line")
-			.attr("class", "xaxis")
-			.attr("transform", function(d,i) {
-				return "translate(" + ((i * barWidth) ) + ", 0)"
-			})
-			.attr("x1", 0)					
-			.attr("x2", 0)
-			.attr("y1", 0)	
-			.attr("y2", cheight);					
+		    .attr('transform', 'translate(' + [barWidth, 0] + ')');							
 		
 		var bars = g.append('g')
-			.attr('class', 'bar-content');
-		
-		bars.selectAll("rect")
-			.data(dataset)
-			.enter()
-			.append("rect")
-			.attr("class", function(d) { return d.name })
-			.classed('carrier-bar', true)
-			.attr("height", 0)
-			.attr("y", function(d) { return cheight; })   											
-			.attr("width", recWidth)
-			.attr("transform", function(d, i) { return "translate(" + ((i * barWidth) + (barWidth/2) - recWidth/2) + ",0)"; });
-			
-			
-		if(errorBars) { 
-			bars.selectAll(".error-bar-top")
-				.data(dataset)
-				.enter()
-				.append("rect")
-				.attr("class", "error-bar-top")
-				.attr("height", 1)
-				.attr("width", recWidth/2)
-				.attr("transform", function(d, i) { 
-					return "translate(" + ((barWidth*i) + (barWidth/2) - (recWidth/4)) + "," + cheight + ")"; 
-				})
-				.style('display', 'none');			
-			
-			bars.selectAll(".error-bar-bottom")
-				.data(dataset)
-				.enter()
-				.append("rect")
-				.attr("class", "error-bar-bottom")
-				.attr("height", 1)
-				.attr("width", recWidth/2)
-				.attr("transform", function(d, i) {
-					return "translate(" + ((barWidth*i) + (barWidth/2) - (recWidth/4)) + "," + cheight + ")"; 
-				})
-				.style('display', 'none');
-				
-			bars.selectAll(".error-bar")
-				.data(dataset)
-				.enter()
-				.append("rect")
-				.attr("class", function(d) { return d.name })
-				.classed("error-bar secondary", true)
-				.attr("width", 2)
-				.attr("transform", function(d, i) { 
-					return "translate(" + ((i * barWidth) + (barWidth/2)) + "," + cheight + ")"; })
-				.attr("height", 0);	
-		};			
-		
-		bars.selectAll(".scoreText")
-            .data(dataset)
-            .enter()
-            .append("text")
-            .attr("class", "scoreText")
-            .attr("text-anchor", "middle")
-            .style("fill", "#555")
-            .style("font-size", "12px")
-            .attr("transform", function(d, i) { 
-		        return "translate(" + ((i * barWidth) + (barWidth/2)) + "," +  cheight + ")"; 
-             })   
-            .text(function(d) { return d.score });
-			      
-	    bars.selectAll(".carrierText")
-            .data(dataset)
-            .enter()
-            .append("text")
-            .style("font-size", "12px")
-            .attr("class", "carrierText")
-            .style("fill", "#555555")
-            .attr("y", cheight + 20)
-            .style("text-anchor", "middle")
-            .attr("transform", function(d, i) { 
-				return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; 
-            })
-            .text(function(d){return d.name;});
-            
-        if(ranks) {
-	        bars.selectAll(".rankCircle")
-                .data(dataset)
-                .enter()
-                .append("circle")
-                .attr('class', "rankCircle")
-                .attr("fill", function(d) {
-					if(d.rank == 1) {
-						return "#555555";
-					} else {
-						return "transparent";
-					}
-				})
-				.attr("stroke", function(d) {
-					if(d.rank == 1) {
-						return "#555555";
-					} else {
-						return "#999999";
-					}
-				})
-                .attr("stroke-width", "1px")
-                .attr("cy", cheight + 40)
-                .attr("r", 10)
-                .attr("transform", function(d, i) { return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; }) 	
-	        
-	    	bars.selectAll(".rankText")
-	            .data(dataset)
-	            .enter()
-	            .append("text")
-	            .style("font-size", "12px")
-	            .attr("class", "rankText")
-	            .style("fill", function(d) {
-					if(d.rank == 1) {
-						return "#FFFFFF";
-					} else {
-						return "#555555";
-					}
-				})
-	            .attr("y", cheight + 44)
-	            .style("text-anchor", "middle")
-	            .attr("transform", function(d, i) { 
-					return "translate(" + (((i * barWidth)) + (barWidth /2 )) + ",0)"; 
-	            })
-	            .text(function(d, i){ return d.rank; });
-        }    
+			.attr('class', 'bar-content');				                    
 	}
 	
 	
